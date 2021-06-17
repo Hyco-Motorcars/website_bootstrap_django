e7acc81f7d96fb40e0114bb7dcaccc9d035cb7ac
Henry Cappel <hencappel1@gmail.com>
2021-06-17 11:28:06 -0500

    completed car modals using bootstrap

---
+++ static/javascript/car-modal.js
@@ -0,0 +1,19 @@
+function change_image(image, counter){
+
+    var container = document.getElementById("main-image"+counter);
+    console.log("main-image"+counter);
+    
+    container.src = image.src;
+    }
+    
+    
+    
+    document.addEventListener("DOMContentLoaded", function(event) {
+    
+    
+    
+    
+    
+    
+    
+    });
