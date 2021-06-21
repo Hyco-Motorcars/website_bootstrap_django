aa3de5b1ff758ffe635180eada96368e9e52612b
Henry Cappel <hencappel1@gmail.com>
2021-06-21 16:28:45 -0500

    finished filter tab and added no vehicles match search condition

--- pages/views.py
+++ pages/views.py
@@ -11,6 +11,32 @@ def homepage_view(request, *args, **kwargs):
 def vehicle_sales_view(request, *args, **kwargs):
     vehicle_qs = Vehicle.objects.all()
 
+    vehicle_make = request.GET.get('searchmake')
+    vehicle_model = request.GET.get('searchmodel')
+    vehicle_year = request.GET.get('searchyear')
+    vehicle_exterior = request.GET.get('searchexterior')
+    vehicle_interior = request.GET.get('searchinterior')
+    vehicle_maxprice = request.GET.get('searchmaxprice')
+
+
+    if vehicle_make != '' and vehicle_make != 'select make' and vehicle_make is not None:
+        vehicle_qs = vehicle_qs.filter(make__icontains=vehicle_make)
+    
+    if vehicle_model != '' and vehicle_model != 'select model' and vehicle_model is not None:
+        vehicle_qs = vehicle_qs.filter(model__icontains=vehicle_model)
+
+    if vehicle_year != '' and vehicle_year != 'select year' and vehicle_year is not None:
+        vehicle_qs = vehicle_qs.filter(year__icontains=vehicle_year)
+
+    if vehicle_exterior != '' and vehicle_exterior != 'select exterior color' and vehicle_exterior is not None:
+        vehicle_qs = vehicle_qs.filter(exterior_color__icontains=vehicle_exterior)
+
+    if vehicle_interior != '' and vehicle_interior != 'select interior color' and vehicle_interior is not None:
+        vehicle_qs = vehicle_qs.filter(interior__icontains=vehicle_interior)
+
+    if vehicle_maxprice != '' and vehicle_maxprice != 'select max price' and vehicle_maxprice is not None:
+        vehicle_qs = vehicle_qs.filter(listing_price__lte=vehicle_maxprice)
+
     context = {
         'all_vehicles': vehicle_qs,
     }
